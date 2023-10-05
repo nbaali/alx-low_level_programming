c417a63853c7f8d12870211f041ead7b9b781b5c
nbaali <maroinejj@gmail.com>
2023-10-05 15:43:57 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #!/bin/bash
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#include <stdio.h>
+int main(void){{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
