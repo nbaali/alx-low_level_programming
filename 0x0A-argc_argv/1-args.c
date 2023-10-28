9a0f0a9c113b57adb19521eb8fc27d13db7e2af5
nbaali <maroinejj@gmail.com>
2023-10-28 05:03:13 -0700

    a

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argv; /* Ignore argv */
-    printf("%d\n", argc - 1);
+(void)argv; /* Ignore argv */
+printf("%d\n", argc - 1);
 
-    return (0);
+return (0);
 }
