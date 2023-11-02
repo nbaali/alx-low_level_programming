383f1b46a50e95d100eb132d72556655f69936b4
nbaali <maroinejj@gmail.com>
2023-11-02 16:55:01 -0700

    a

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int is_digit(char c) {
-	    return (c >= '0' && c <= '9');
+/**
+ *  * main - multiplies two positive numbers
+ *   * @argc: n arguments
+ *    * @argv: args
+ *     * Return: int
+ *     **/
+
+int main(int argc, char *argv[])
+{
+		unsigned long mul;
+			int i, j;
+
+				if (argc != 3)
+						{
+									printf("Error\n");
+											exit(98);
+												}
+
+					for (i = 1; i < argc; i++)
+							{
+										for (j = 0; argv[i][j] != '\0'; j++)
+													{
+																	if (argv[i][j] > 57 || argv[i][j] < 48)
+																					{
+																										printf("Error\n");
+																														exit(98);
+																																	}
+																			}
+											}
+						mul = atol(argv[1]) * atol(argv[2]);
+							printf("%lu\n", mul);
+								return (0);
 }
-
-int str_to_int(char *str) {
-	    int result = 0;
-	        for (int i = 0; str[i]; i++) {
-			        if (!is_digit(str[i])) {
-					            printf("Error\n");
-						                exit(98);
-								        }
-				        result = result * 10 + (str[i] - '0');
-					    }
-		    return result;
-}
-
-int main(int argc, char *argv[]) {
-	    if (argc != 3) {
-		            printf("Error\n");
-			            return (98);
-				        }
-
-	        int num1 = str_to_int(argv[1]);
-		    int num2 = str_to_int(argv[2);
-		        int result = num1 * num2;
-
-			    printf("%d\n", result);
-			        return (0);
-}
-
